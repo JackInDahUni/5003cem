dddc44d9d96bf56e4d0292fea9d3cda273794933
Alan Jack Perkins <perkinsa2@coventry.ac.uk>
2024-02-08 15:20:59 +0000

    Week 4 assessed task is completed

--- W4ATask.py
+++ W4ATask.py
@@ -1,11 +1,11 @@
-def selection_Sort(A):
-	for i in range(len(A)-1):
-		minn = i
-		j = i+1
-		for j in range (len(A)):
-			if A[j] < A[minn]:
-				minn = j
-		swap(A,i,minn)
-	return A
+def selection_Sort(A):					# defines the selection sort with the argument list A
+	for i in range(len(A)-1):			# i itterates through the list
+		minn = i						# the minn variable is then assigned to i
+		j = i+1							# the variable j is assigned to the value i + 1
+		for j in range (i, len(A)):		# j itterates through the list starting from i
+			if A[j] < A[minn]:			# if element j is less than element minn
+				minn = j				# minn is equal to j
+		A[i], A[minn] = A[minn], A[i]	# element i is swapped with element minn
+	return A							# returns the sorted list
 
-def swap(A,i,minn):
+print(selection_Sort([11, 22, 14, 67, 2, 9]))
